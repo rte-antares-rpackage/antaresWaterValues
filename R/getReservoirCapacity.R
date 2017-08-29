948f508b50fdae53b56b6ef5b3453896696aa14b
pvictor <perrier.victor@gmail.com>
2017-08-29 18:02:48 +0200

    add getReservoirCapacity

---
+++ R/getReservoirCapacity.R
@@ -0,0 +1,29 @@
+#' Get the reservoir capacity for an area
+#'
+#' @param area An area.
+#' @param force If "reservoir management" is disabled, return anyway the reservoir capacity?
+#' @param opts
+#'   List of simulation parameters returned by the function
+#'   \code{antaresRead::setSimulationPath}  
+#'
+#' @return the reservoir capacity (in Wh), or \code{NULL} if none.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' 
+#' getReservoirCapacity("fr")
+#' 
+#' }
+getReservoirCapacity <- function(area, force = FALSE, opts = antaresRead::simOptions()) {
+  assertthat::assert_that(class(opts) == "simOptions")
+  if (!area %in% antaresRead::getAreas(opts = opts)) 
+    stop("Not a valid area!")
+  hydro_ini <- antaresEditObject::readIniFile(file.path(opts$inputPath, "hydro", "hydro.ini"))
+  if (isTRUE(hydro_ini$reservoir[[area]]) | force) {
+    reservoir_capacity <- hydro_ini[["reservoir capacity"]][[area]]
+  } else {
+    reservoir_capacity <- NULL
+  }
+  reservoir_capacity
+}

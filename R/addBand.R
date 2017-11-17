8740e56847e8136e484a3956b00ea6f83f6e2904
pvictor <perrier.victor@gmail.com>
2017-11-17 14:42:14 +0100

    spline interpolation

--- R/addBand.R
+++ R/addBand.R
@@ -2,14 +2,15 @@
 #' Add Band to Water Values
 #'
 #' @param vu Water values from \code{meanGridLayer}
-#' @param states Level of states
+#' @param states Level of states.
+#' @param type Perform a linear or a spline interpolation.
 #' @param bandwidth Ith of the band to add.
 #' @param failure_cost Cost of failure
 #'
 #' @return A vector of water values with a band
 #' @export
 #' 
-#' @importFrom zoo na.approx
+#' @importFrom zoo na.approx na.spline
 #' @importFrom data.table first last
 #'
 #' @examples
@@ -18,8 +19,9 @@
 #' # TODO
 #' 
 #' }
-addBand <- function(vu, states, bandwidth = 100, failure_cost = 3000) {
+addBand <- function(vu, states, type = c("linear", "spline"), bandwidth = 100, failure_cost = 3000) {
   
+  type <- match.arg(type)
   num_equal <- function(x, y, tol = sqrt(.Machine$double.eps)) {
     abs(x - y) < tol
   }
@@ -35,19 +37,32 @@ addBand <- function(vu, states, bandwidth = 100, failure_cost = 3000) {
   states_below <- data[!is.na(vu_band), data.table::first(states)] + c(-bandwidth, bandwidth)/100
   states_below[1] <- max(states_below[1], min(data$states))
   states_below[2] <- min(states_below[2], max(data$states))
-  n_nas <- length(seq(from = states_below[1], to = states_below[2], by = diff_states))
-  val <- data[num_equal(states, states_below[2]), vu_band]
-  vals <- zoo::na.approx(c(failure_cost, rep_len(NA, n_nas - 2), val))
-  data[states >= states_below[1] & states <= states_below[2], vu_band := vals]
+  # n_nas <- length(seq(from = states_below[1], to = states_below[2], by = diff_states))
+  # val <- data[num_equal(states, states_below[2]), vu_band]
+  # vals <- zoo::na.approx(c(failure_cost, rep_len(NA, n_nas - 2), val))
+  # data[states >= states_below[1] & states <= states_below[2], vu_band := vals]
+  data <- data[states > states_below[1] & states < states_below[2], vu_band := NA]
+  data <- data[num_equal(states, states_below[1]), vu_band := failure_cost]
   
   # above
   states_above <- data[!is.na(vu_band), data.table::last(states)] + c(-bandwidth, bandwidth)/100
   states_above[1] <- max(states_above[1], min(data$states))
   states_above[2] <- min(states_above[2], max(data$states))
-  n_nas <- length(seq(from = states_above[1], to = states_above[2], by = diff_states))
-  val <- data[num_equal(states, states_above[1]), vu_band]
-  vals <- zoo::na.approx(c(val, rep_len(NA, n_nas - 2), 0))
-  data[states >= states_above[1] & states <= states_above[2], vu_band := vals]
+  # n_nas <- length(seq(from = states_above[1], to = states_above[2], by = diff_states))
+  # val <- data[num_equal(states, states_above[1]), vu_band]
+  # vals <- zoo::na.approx(c(val, rep_len(NA, n_nas - 2), 0))
+  # data[states >= states_above[1] & states <= states_above[2], vu_band := vals]
+  data <- data[states > states_above[1] & states < states_above[2], vu_band := NA]
+  data <- data[num_equal(states, states_above[2]), vu_band := 0]
+  
+  # Interpolation
+  if (type == "linear") {
+    data <- data[, vu_band := zoo::na.approx(vu_band, na.rm = FALSE)]
+  } else {
+    data <- data[, vu_band := exp(zoo::na.spline(log1p(vu_band), na.rm = FALSE, method = "monoH.FC")) - 1]
+    data <- data[states > states_above[2], vu_band := NA]
+    data <- data[states < states_below[1], vu_band := NA]
+  }
   
   # end
   return(data$vu_band)
--- R/vizWVbyWeek.R
+++ R/vizWVbyWeek.R
@@ -2,7 +2,7 @@
 #'
 #' @param value_node Result from \code{meanGridLayer}.
 #' @param week Week to visualize
-#' @param add_band Add band around water values
+#' @param add_band Add band around water values, choice between 'none', 'linear', 'spline' or 'both'. 
 #' @param ... Parametters passed to \code{\link{addBand}}.
 #'
 #' @return A ggplot2 object
@@ -16,14 +16,15 @@
 #' # TODO
 #' 
 #' }
-vizWVbyWeek <- function(value_node, week, add_band = FALSE, ...) {
+vizWVbyWeek <- function(value_node, week, add_band = 'none', ...) {
+  add_band <- match.arg(add_band, c("none", "linear", "spline", "both"))
   args <- list(...)
   value_node <- copy(value_node)
   value_node <- value_node[weeks == week]
   value_node <- value_node[!(!is.na(vu) & is.finite(vu)), vu := NA]
   p <- ggplot2::ggplot(data = value_node)
   p <- p + ggplot2::geom_point(ggplot2::aes(x = states, y = vu))
-  if (add_band) {
+  if (add_band == "linear") {
     value_node$band <- addBand(
       vu = value_node$vu, 
       states = value_node$states, 
@@ -31,7 +32,35 @@ vizWVbyWeek <- function(value_node, week, add_band = FALSE, ...) {
       failure_cost = args$failure_cost %||% 3000
     )
     p <- p %+% value_node
-    p <- p + ggplot2::geom_line(ggplot2::aes(x = states, y = band))
+    p <- p + ggplot2::geom_line(ggplot2::aes(x = states, y = band), color = "firebrick")
+  } else if (add_band == "spline") {
+    value_node$band <- addBand(
+      vu = value_node$vu, 
+      states = value_node$states, 
+      type = "spline",
+      bandwidth = args$bandwidth %||% 100, 
+      failure_cost = args$failure_cost %||% 3000
+    )
+    p <- p %+% value_node
+    p <- p + ggplot2::geom_line(ggplot2::aes(x = states, y = band), color = "steelblue")
+  } else if (add_band == "both") {
+    value_node$band_lin <- addBand(
+      vu = value_node$vu, 
+      states = value_node$states, 
+      type = "linear",
+      bandwidth = args$bandwidth %||% 100, 
+      failure_cost = args$failure_cost %||% 3000
+    )
+    value_node$band_spl <- addBand(
+      vu = value_node$vu, 
+      states = value_node$states, 
+      type = "spline",
+      bandwidth = args$bandwidth %||% 100, 
+      failure_cost = args$failure_cost %||% 3000
+    )
+    p <- p %+% value_node
+    p <- p + ggplot2::geom_line(ggplot2::aes(x = states, y = band_lin), color = "firebrick")
+    p <- p + ggplot2::geom_line(ggplot2::aes(x = states, y = band_spl), color = "steelblue")
   }
   p <- p + ggplot2::theme_minimal()
   p

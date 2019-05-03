4cc7ef25e15eda739f9c3258a8d93b0fe66ea202
pvictor <perrier.victor@gmail.com>
2019-05-03 10:46:46 +0200

    update reading reservoir level with v7

--- R/readReservoirLevels.R
+++ R/readReservoirLevels.R
@@ -10,6 +10,7 @@
 #' @import data.table
 #' @importFrom assertthat assert_that
 #' @importFrom antaresRead getAreas
+#' @importFrom antaresEditObject is_antares_v7
 #' @importFrom zoo na.approx
 #' @importFrom utils head
 #'
@@ -17,8 +18,30 @@
 #' @export
 #'
 # @examples
-readReservoirLevels <- function(area, timeStep = "weekly", byReservoirCapacity = TRUE, opts = antaresRead::simOptions()) {
+readReservoirLevels <- function(area, 
+                                timeStep = "weekly",
+                                byReservoirCapacity = TRUE,
+                                opts = antaresRead::simOptions()) {
   assertthat::assert_that(class(opts) == "simOptions")
+  if (is_antares_v7(opts = opts)) {
+    readReservoirLevelsV7(
+      area = area,
+      timeStep = timeStep, 
+      byReservoirCapacity = byReservoirCapacity, 
+      opts = opts
+    )
+  } else {
+    readReservoirLevelsV6(
+      area = area, 
+      timeStep = timeStep, 
+      byReservoirCapacity = byReservoirCapacity,
+      opts = opts
+    )
+  }
+}
+
+
+readReservoirLevelsV6 <- function(area, timeStep = "weekly", byReservoirCapacity = TRUE, opts = antaresRead::simOptions()) {
   timeStep <- match.arg(arg = timeStep, choices = c("weekly", "monthly"))
   if (!area %in% antaresRead::getAreas(opts = opts)) 
     stop("Not a valid area!")
@@ -30,7 +53,7 @@ readReservoirLevels <- function(area, timeStep = "weekly", byReservoirCapacity =
     reservoir <- data.table::fread(input = path, col.names = vars)
   }
   full_year <- data.table(
-    date = seq.Date(from = as.Date(opts$start), by = "day", length.out = 364)
+    date = seq.Date(from = as.Date(opts$start), by = "day", length.out = nrow(reservoir))
   )
   reservoir <- reservoir[, date := full_year[format(date, "%d") == "01"][order(format(date, "%m")), c(date)]]
   if (timeStep == "monthly") {
@@ -43,17 +66,62 @@ readReservoirLevels <- function(area, timeStep = "weekly", byReservoirCapacity =
     utils::head(zoo::na.approx(c(x, x[1]), na.rm = FALSE), -1)
   }
   reservoir <- reservoir[, (vars) := lapply(.SD, interpolation), .SDcols = vars]
-  reservoir <- reservoir[, timeId := rep(seq_len(52), each = 7)]
+  reservoir <- reservoir[, timeId := c(rep(seq_len(52), each = 7), 52)]
   reservoir <- reservoir[, lapply(.SD, mean), by = timeId, .SDcols = vars]
   if (byReservoirCapacity) {
     reservoirCapacity <- getReservoirCapacity(area = area, opts = opts)
     if (!is.null(reservoirCapacity)) {
       vars <- c("level_low", "level_avg", "level_high")
       reservoir <- reservoir[, 
-        (vars) := lapply(.SD, function(x) {x * reservoirCapacity/1e6}),
-        .SDcols = vars
-        ]
+                             (vars) := lapply(.SD, function(x) {x * reservoirCapacity/1e6}),
+                             .SDcols = vars
+                             ]
     }
   }
-  reservoir
+  reservoir[]
 }
+
+
+
+readReservoirLevelsV7 <- function(area, timeStep = "weekly", byReservoirCapacity = TRUE, opts = antaresRead::simOptions()) {
+  timeStep <- match.arg(arg = timeStep, choices = c("weekly", "monthly", "daily"))
+  if (!area %in% antaresRead::getAreas(opts = opts)) 
+    stop("Not a valid area!")
+  path <- file.path(opts$inputPath , sprintf("hydro/common/capacity/reservoir_%s.txt", area))
+  vars <- c("level_low", "level_avg", "level_high")
+  if (!file.exists(path) || file.size(path) == 0) {
+    reservoir <- data.table::data.table(matrix(rep(0, 3*365), ncol = 3, dimnames = list(NULL, vars)))
+  } else {
+    reservoir <- data.table::fread(input = path, col.names = vars)
+  }
+  full_year <- data.table(
+    date = seq.Date(from = as.Date(opts$start), by = "day", length.out = nrow(reservoir))
+  )
+  reservoir <- reservoir[, date := full_year[order(format(date, "%m")), c(date)]]
+  if (timeStep == "daily") {
+    setnames(x = reservoir, old = "date", new = "timeId")
+  }
+  if (timeStep == "monthly") {
+    data.table::setcolorder(x = reservoir, neworder = c("date", vars))
+    reservoir <- reservoir[order(date)]
+    reservoir[, timeId := format(date, format = "%Y-%m-01")]
+    reservoir <- reservoir[, lapply(.SD, mean), by = timeId]
+  }
+  if (timeStep == "weekly") {
+    reservoir <- reservoir[, timeId := c(rep(seq_len(52), each = 7), 52)]
+    reservoir <- reservoir[, lapply(.SD, mean), by = timeId, .SDcols = vars]
+  }
+  if (byReservoirCapacity) {
+    reservoirCapacity <- getReservoirCapacity(area = area, opts = opts)
+    if (!is.null(reservoirCapacity)) {
+      vars <- c("level_low", "level_avg", "level_high")
+      reservoir <- reservoir[
+        , (vars) := lapply(.SD, function(x) {x * reservoirCapacity/1e6}),
+        .SDcols = vars
+        
+      ]
+    }
+  }
+  reservoir[]
+}
+

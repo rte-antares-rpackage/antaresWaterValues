b5c0f9cf022825b5eceb8f8da434410c2fe6cd05
Etienne <etienne.sanchez@dreamrs.fr>
2019-11-26 17:50:46 +0100

    Tests for resetHydroStorage() and restoreHydroStorage()

--- tests/testthat/test-hydroStorage.R
+++ tests/testthat/test-hydroStorage.R
@@ -23,3 +23,26 @@ test_that("resetHydroStorage() creates a backup file and sets mod.txt to zero", 
   hydro_storage <- utils::read.table(file = file.path(opts$inputPath, "hydro", "series", "b", "mod.txt"))
   expect_equal(hydro_storage, data.frame(V1 = rep(0, 365)))
 })
+
+test_that("resetHydroStorage() creates a hydro storage file when needed", {
+  storage_file <- file.path(opts$inputPath, "hydro", "series", "c", "mod.txt")
+  file.remove(storage_file)
+  expect_message(resetHydroStorage("c"), regexp = "No hydro storage series for this area, creating one")
+  file.exists(storage_file)
+})
+
+
+test_that("Error when invalid area in restoreHydroStorage()", {
+  expect_error(restoreHydroStorage("z"), regexp = "not a valid area")
+})
+
+test_that("restoreHydroStorage() prints an appropriate message when there is no backup file", {
+  expect_message(restoreHydroStorage("a"), regexp = "No backup found")
+})
+
+test_that("restoreHydroStorage() replaces the storage file by the backup file and removes the backup file", {
+  backup <- readLines(file.path(opts$inputPath, "hydro", "series", "b", "mod_backup.txt"))
+  restoreHydroStorage("b")
+  new_storage_file <- file.path(opts$inputPath, "hydro", "series", "b", "mod.txt")
+  expect_identical(backup, readLines(new_storage_file))
+})

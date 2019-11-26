617ad4d8268a004b336a4294ed50cf466e227ef3
Etienne <etienne.sanchez@dreamrs.fr>
2019-11-26 14:55:12 +0100

    Completed test-getReservoirCapacity.R

--- tests/testthat/test-getReservoirCapacity.R
+++ tests/testthat/test-getReservoirCapacity.R
@@ -15,4 +15,6 @@ opts <- antaresRead::setSimulationPath(studyPath, "input")
 
 test_that("getReservoirCapacity() works", {
   expect_equal(getReservoirCapacity("c"), 1234)
+  expect_error(getReservoirCapacity("z"), regexp = "Not a valid area")
+  expect_null(getReservoirCapacity("b"))
 })

7d63525f1a5b48e322dd34e0678a065acee7c7dd
Etienne <etienne.sanchez@dreamrs.fr>
2019-11-27 11:27:30 +0100

    Added another test for toAntaresFormat()

--- tests/testthat/test-toAntaresFormat.R
+++ tests/testthat/test-toAntaresFormat.R
@@ -4,3 +4,20 @@ test_that("toAntaresFormat() creates a 365*101 numeric matrix", {
   expect_equal(dim(toAntaresFormat(fake_water_values)), c(365, 101))
   expect_true(is.numeric(m))
 })
+
+test_that("toAntaresFormat() works when water value is a constant", {
+  wv <- data.table::data.table(
+    weeks = rep(1:52, each = 357),
+    statesid = rep(357:1, 52),
+    states = seq(0, length.out = 357),
+    vu = rep(80, 52*357)
+  )
+  
+  mat <- toAntaresFormat(wv)
+  dimnames(mat) <- NULL
+  
+  expect_equal(
+    mat,
+    matrix(rep(80, 365*101), nrow = 365)
+  )
+})

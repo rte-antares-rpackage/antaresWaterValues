22ef381360403732ac262e724fd034b0af21700a
dhia-gharsallaoui <safedhia@gmail.com>
2021-07-15 15:11:52 +0200

    in construction

--- R/post_process.R
+++ R/post_process.R
@@ -1,6 +1,7 @@
 monotonic_VU <- function(results,noise_ratio=0.001)
 
-{  results[vu==Inf|is.na(vu),vu:=NaN]
+{
+  results[vu==Inf|is.na(vu),vu:=NaN]
   qm <- quantile(results$vu,0.95,na.rm = TRUE)
   temp <- results[vu>1.5*qm]
 
@@ -9,7 +10,7 @@ monotonic_VU <- function(results,noise_ratio=0.001)
   }
   temp <- NULL
 
-  for (i in 1:52){
+  for (i in 1:53){
 
     temp <- results[weeks==i]
     maxi <- max(temp$vu,na.rm = TRUE)
@@ -29,44 +30,10 @@ monotonic_VU <- function(results,noise_ratio=0.001)
       M <- j
 
     }
-      temp$vu[m:M]<- temp$vu[m:M][order(temp$vu[m:M],decreasing = t)]
 
 
-
-
-
-
-
-
-
-
-
-    # for (j in 1:nrow(temp)){
-    #   vu <- temp$vu[j]
-    #
-    #   if (is.na(vu)) next
-    #
-    #   if (counter==0){
-    #     temp$vu[j] <- maxi
-    #     counter <- 1
-    #     next
-    #   }
-    #
-    #
-    #  if (j<nrow(temp)&!is.na(temp$vu[j+1]))
-    #  {
-    #    if (vu>temp$vu[j-1]){
-    #      if (temp$vu[j+1]<=temp$vu[j-1])
-    #       {  temp$vu[j] <- mean(temp$vu[j+1],temp$vu[j-1])
-    #      }else
-    #       temp$vu[j] <-  temp$vu[j-1] - ((temp$vu[j-2]-temp$vu[j-1]))/10
-    #      }
-    #    }
-    #
-    #
-    # }
-
-    results[weeks==i,vu :=temp$vu]
+      temp$vu[m:M]<- temp$vu[m:M][order(temp$vu[m:M],decreasing = TRUE)]
+      results[weeks==i,vu :=temp$vu]
   }
 
   return(results)
@@ -83,14 +50,14 @@ post_process <- function(results){
   results[vu==Inf|is.na(vu),vu:=NaN]
   maxid <- max(results$statesid)
   q3 <- quantile(results$statesid,0.75)
-  for (i in 1:52){
+  for (i in 1:53){
 
 
     maxi <- max(results[weeks==i]$vu,na.rm = TRUE)
     mini <- min(results[weeks==i]$vu,na.rm = TRUE)
 
     results[weeks==i&!is.finite(vu)&states>=level_high,vu:=interp_up(mini,statesid)]
-    results[weeks==i&!is.finite(vu)&states<=level_low,vu:=interp_down(mini,statesid,q3)]
+    results[weeks==i&!is.finite(vu)&states<=level_low,vu:=interp_down(maxi,statesid,q3)]
     results[weeks==i&!is.finite(vu),vu:=mini]
 
   }
@@ -101,7 +68,7 @@ return(results)
 
 interp_up <- function(mini,statesid){
 
-  mini*(1-(exp(-statesid)))
+  mini*(1+(exp(-statesid)))*-1
 
 }
 

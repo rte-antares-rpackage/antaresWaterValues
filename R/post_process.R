2a5a07d7383785d12f622a2d00ea2cda7bd50154
dhia-gharsallaoui <safedhia@gmail.com>
2021-08-05 15:32:24 +0200

    add full imputation option and improve the function

--- R/post_process.R
+++ R/post_process.R
@@ -45,13 +45,14 @@ monotonic_VU <- function(results,noise_ratio=0.001)
 
 
 
-post_process <- function(results,down_cost=3000,impute_method='pmm'){
+post_process <- function(results,down_cost=3000,full_imputation=FALSE,impute_method='pmm'){
 
-  results[vu==Inf|is.na(vu),vu:=NaN]
+  results[vu==Inf|is.na(vu),vu:=NA]
   maxid <- max(results$statesid)
   q3 <- quantile(results$statesid,0.75)
 
-  results[vu>down_cost,vu:=down_cost]
+  results[abs(vu)>down_cost,vu:=NA]
+  # results[vu<-down_cost,vu:=NA]
 
 
   cat("Prepare Water Values:\n")
@@ -59,30 +60,45 @@ post_process <- function(results,down_cost=3000,impute_method='pmm'){
   pb <- txtProgressBar(min = 0, max = 51, style = 3)
 
    for (i in 1:52){
+    if (i==7) browser()
     maxi <- max(results[weeks==i]$vu,na.rm = TRUE)
     mini <- min(results[weeks==i]$vu,na.rm = TRUE)
 
-    # results[weeks==i&!is.finite(vu)&states>=level_high,vu:=interp_up(mini,statesid)]
-    results[weeks==i&!is.finite(vu)&states>=level_high,vu:=-down_cost]
+    if(!full_imputation){
+      # results[weeks==i&!is.finite(vu)&states>=level_high,vu:=interp_up(mini,statesid)]
+      results[weeks==i&!is.finite(vu)&states>=level_high,vu:=0]
 
-    if (down_cost>0){
-      results[weeks==i&!is.finite(vu)&states<=level_low,vu:=down_cost]
-      results[weeks==i&states<=level_low,vu:=down_cost]
+      if (down_cost>0){
+        results[weeks==i&!is.finite(vu)&states<=level_low,vu:=down_cost]
+        results[weeks==i&states<=level_low,vu:=down_cost]
+      }
+      results[weeks==i&!is.finite(vu)&states<=level_low,vu:=interp_down(maxi,statesid,q3)]
+    }else{
+      results[weeks==i&states>level_high,vu:=NA]
+      results[weeks==i&states<level_low,vu:=NA]
+      results[weeks==i&statesid==maxid,vu:=down_cost]
+      results[weeks==i&statesid==1,vu:=-down_cost]
+
+
+
     }
-    results[weeks==i&!is.finite(vu)&states<=level_low,vu:=interp_down(maxi,statesid,q3)]
+
 
     temp <- results[weeks==i]
-    temp <- dplyr::select(temp,states,vu)
-    if (any(is.na(temp))){
+    temp <- dplyr::select(temp,statesid,vu)
+        if (any(is.na(temp))){
 
-      imputed_Data <- mice(temp, m=1, maxit = 50, method = impute_method, seed = 500,print = F)
+      imputed_Data <- mice(temp, m=1, maxit = 50, method = impute_method,
+                           seed = 500,print = F)
       completeData <- complete(imputed_Data,1)
-      results[weeks==i]$vu <- completeData$vu
+
+
+      results[weeks==i]$vu <- sort(completeData$vu,decreasing = T)
+
 
     }
     setTxtProgressBar(pb = pb, value = i)
    }
-  results[(vu< (-down_cost)&vu<0),vu:=-down_cost]
   close(pb)
 return(results)
 

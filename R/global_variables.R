ec727fa645a4716dc0a59d0a6b32dfd8f9edc9db
dhia-gharsallaoui <safedhia@gmail.com>
2021-09-13 20:27:55 +0200

    add global variables for check warnings

---
+++ R/global_variables.R
@@ -0,0 +1,15 @@
+# The goal of the following lines is only to remove many useless warnings in
+# R CMD CHECK: "no visible binding for global variable 'XXX'".
+# They come from the use of the data.table syntax and from others syntax (loop,ggplot,..).
+
+utils::globalVariables(
+  c("timeId", "area", "hydroStorage", "value_node","i","years","mcYear","time",
+    "hydroStorage","tsId","weeks","statesid","level_low","level_high",
+    "value_node_dif","states_dif","vu","hstorPMaxHigh","sim_name","value",
+    "Reservoir_percent","Bellman_Value","value_node_dif","hydroStorage",
+    "Water_Flow","legend","variable","value","area","H. STOR","generation",
+    "day","H. LEV","Turbining capacity","Reward","Turbining transistion",
+    "Reward transition","states","hstorPMaxHigh","generatingMaxPower",
+    "states_percent","i.statesid","i.value_node","i.value_node_dif","i.vu",
+    "states_percent","vu_band","addBand","MC_year","nvu")
+)

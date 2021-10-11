6041fac5565f7e0b8ec152d41c81afb36300cade
dhia-gharsallaoui <safedhia@gmail.com>
2021-10-11 15:26:44 +0200

    No need to shift values by a week because it's done in Antares

--- R/math_functions.R
+++ R/math_functions.R
@@ -15,13 +15,13 @@ build_data_watervalues <- function(watervalues,inaccessible_states,statesdt,rese
   value_nodes_dt <- value_node_gen(watervalues,inaccessible_states,statesdt,reservoir)
   value_nodes_dt <- value_nodes_dt[value_nodes_dt$weeks!=53,]
   inacc <- is.finite(value_nodes_dt$value_node)
-  temp1 <- value_nodes_dt[weeks==1]$vu
-  temp2 <- value_nodes_dt[weeks>1&weeks<53]$vu
-  value_nodes_dt[weeks==52]$vu <- temp1
-  value_nodes_dt[weeks<52]$vu <- temp2
+  # temp1 <- value_nodes_dt[weeks==1]$vu
+  # temp2 <- value_nodes_dt[weeks>1&weeks<53]$vu
+  # value_nodes_dt[weeks==52]$vu <- temp1
+  # value_nodes_dt[weeks<52]$vu <- temp2
 
   value_nodes_dt$inacc <- inacc
-  value_nodes_dt[inacc==F,vu:=NaN]
+  value_nodes_dt[,vu_corr:=inacc*vu]
   print(waterValuesViz(value_nodes_dt,0.99))
   return(value_nodes_dt)
 

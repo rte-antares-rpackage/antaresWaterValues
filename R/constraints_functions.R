b136d26d3cbea22b72bd199a634c6253907003e2
dhia-gharsallaoui <safedhia@gmail.com>
2021-12-01 19:04:56 +0100

    new pumping model

--- R/constraints_functions.R
+++ R/constraints_functions.R
@@ -46,23 +46,21 @@ simulation_names_values <- function(area,simulation_name,nb_disc_stock,
 #' This function disable binding constraints for \code{runWaterValuesSimulation}
 #' @param constraint_value the value of the constraint
 #' @param name_bc the name of the constraint.
+#' @param pumping Boolean. True to take into account the pumping.
 #' @param opts
 #'   List of simulation parameters returned by the function
 #'   \code{antaresRead::setSimulationPath}
 #' @importFrom antaresEditObject editBindingConstraint
 #' @export
 
-disable_constraint <- function(constraint_value,name_bc,opts){
+disable_constraint <- function(constraint_value,name_bc,pumping=F,opts){
 
-  opts_i <- antaresEditObject::editBindingConstraint(name = name_bc, opts = opts,enabled = FALSE)
-
-  if(constraint_value<0){
-    mod <- "Pump"
-  }else{ mod <- "Turb"}
-
-  opts_i <- antaresEditObject::editBindingConstraint(name = mod, opts = opts,enabled = FALSE)
-
- return(opts_i)
+  opts <- antaresEditObject::editBindingConstraint(name = name_bc, opts = opts,enabled = FALSE)
+  opts <- antaresEditObject::editBindingConstraint(name = "Turb", opts = opts,enabled = FALSE)
+  if(pumping){
+  opts <- antaresEditObject::editBindingConstraint(name = "Pump", opts = opts,enabled = FALSE)
+  }
+ return(opts)
 }
 
 
@@ -75,42 +73,18 @@ disable_constraint <- function(constraint_value,name_bc,opts){
 #' This function generate binding constraints for \code{runWaterValuesSimulation}
 #' @param constraint_value the value of the constraint
 #' @param coeff the sens of the constraint notation in Antares.
-#' @param name_bc the name of the constraint.
+#' @param name_constraint the name of the constraint.
+#' @param efficiency in [0,1]. efficient ratio of pumping.
 #' @param opts
 #'   List of simulation parameters returned by the function
 #'   \code{antaresRead::setSimulationPath}
 #' @export
 
 
-generate_constraints <- function(constraint_value,coeff,name_bc,opts){
+generate_constraints <- function(constraint_value,coeff,name_constraint,efficiency=0.75,opts){
 
-  if(constraint_value<0){
-    # Implement the flow sens in the study
 
-    opts <- antaresEditObject::createBindingConstraint(
-      name = "Pump",
-      enabled = TRUE,
-      operator = "greater",
-      coefficients = -coeff,
-      opts = opts,
-      overwrite = TRUE,
-      timeStep = "hourly"
-    )
-
-    # Implement binding constraint
-
-    opts <- antaresEditObject::createBindingConstraint(
-      name = name_bc,
-      values = data.frame(equal = rep(-constraint_value, times = 366)),
-      enabled = TRUE,
-      timeStep = "weekly",
-      operator = "equal",
-      overwrite = TRUE,
-      coefficients = -coeff,
-      opts = opts
-    )
-
-  }else{
+  if(length(coeff)==1){
 
     opts <-  antaresEditObject::createBindingConstraint(
       name = "Turb",
@@ -123,7 +97,7 @@ generate_constraints <- function(constraint_value,coeff,name_bc,opts){
     )
 
     opts <- antaresEditObject::createBindingConstraint(
-      name = name_bc,
+      name = name_constraint,
       values = data.frame(less = rep(constraint_value, times = 366)),
       enabled = TRUE,
       timeStep = "weekly",
@@ -131,10 +105,58 @@ generate_constraints <- function(constraint_value,coeff,name_bc,opts){
       overwrite = TRUE,
       coefficients = coeff,
       opts = opts)
+  }else{
+
+
+    # Implement the flow sens in the study Pumping
+
+    opts <- antaresEditObject::createBindingConstraint(
+      name = "Pump",
+      enabled = TRUE,
+      operator = "greater",
+      coefficients = -coeff[2],
+      opts = opts,
+      overwrite = TRUE,
+      timeStep = "hourly"
+    )
+
+    # Implement the flow sens in the study Turbining
+
+    opts <-  antaresEditObject::createBindingConstraint(
+      name = "Turb",
+      enabled = TRUE,
+      operator = "greater",
+      coefficients = coeff[1],
+      opts = opts,
+      overwrite = TRUE,
+      timeStep = "hourly"
+    )
+
+
+
+    # Implement binding constraint
+
+    opts <- antaresEditObject::createBindingConstraint(
+      name = name_constraint,
+      values = data.frame(equal = rep(constraint_value, times = 366)),
+      enabled = TRUE,
+      timeStep = "weekly",
+      operator = "equal",
+      overwrite = TRUE,
+      coefficients = c(coeff[1],efficiency*coeff[2]),
+      opts = opts)
   }
 
 
-}
+    return(opts)
+    }
+
+
+
+
+
+
+
 
 
 
@@ -169,4 +191,17 @@ constraint_generator <- function(area,nb_disc_stock,pumping=F,pumping_efficiency
   }
 
   return(constraint_values)
+}
+
+
+
+
+generate_link_coeff <- function(area,fictive_area){
+
+  if (match(area, sort(c(area, fictive_area))) == 1) {
+    coeff <- stats::setNames(-1, paste(area, fictive_area, sep = "%"))
+  } else {
+    coeff <- stats::setNames(1, paste(fictive_area, area, sep = "%"))
+  }
+  return(coeff)
 }

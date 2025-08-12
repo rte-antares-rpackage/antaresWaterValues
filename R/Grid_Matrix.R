079582c0857f8f3c93e6eb9a4bed09ae156f47dd
Juliette-Gerbaux <juliette.gerbaux@rte-france.com>
2025-08-12 11:49:48 +0200

    Update documentation

--- R/Grid_Matrix.R
+++ R/Grid_Matrix.R
@@ -1,7 +1,7 @@
 #' Compute Bellman values
 #'
 #' Compute Bellman values and water values with dynamic programming based on reward functions computed with \code{get_Reward()}.
-#' Mode information about this function in \code{vignette("Grid_Matrix parameters", package = "antaresWaterValues")}.
+#' Mode information about this function in \code{vignette("grid_Matrix-parameters")}.
 #'
 #' @inheritParams runWaterValuesSimulation
 #' @inheritParams get_Reward
